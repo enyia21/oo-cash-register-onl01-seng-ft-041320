27d53e7842603b74fae996469ac70237b33b6d10
Chibuisi Enyia <enyia21@gmail.com>
2020-04-26 22:04:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
 
+  attr_accessor :discount, :total :title :price :quantity
+
+  def initialize(discount=0)
+    @total = 0
+    self.discount = discount
+  end
+
+  def total
+    @total
+  end
+
+  def add_item
+
+  end
+  # def describe
+  #   @total = 0
+  # end
+
+end
